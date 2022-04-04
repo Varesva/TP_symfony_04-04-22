1c370e881e5d935a7647870661f72af1dd05614f
Varesva <tavaresjustine.pro@mail.com>
2022-04-04 20:39:07 +0200

    main

---
+++ Questions_du_TP.php
@@ -0,0 +1,7 @@
+Les fonctions d’agrégation servent à effectuer des statistiques.
+SELECT COUNT(*) FROM table1;
+
+Clé étrangère
+Jointure interne
+Jointure externe
+Donnez un exemple d’utilisation avec le code associé
--- src/Controller/ProfileCategorieController.php
+++ src/Controller/ProfileCategorieController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Categorie;
-use App\Form\CategorieType;
+use App\Form\Categorie1Type;
 use App\Repository\CategorieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,71 +11,71 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/categorie")
+ * @Route("/profile/categorie")
  */
-class CategorieController extends AbstractController
+class ProfileCategorieController extends AbstractController
 {
     /**
-     * @Route("/", name="app_categorie_index", methods={"GET"})
+     * @Route("/", name="app_profile_categorie_index", methods={"GET"})
      */
     public function index(CategorieRepository $categorieRepository): Response
     {
-        return $this->render('categorie/index.html.twig', [
+        return $this->render('profile_categorie/index.html.twig', [
             'categories' => $categorieRepository->findAll(),
         ]);
     }
 
     /**
-     * @Route("/new", name="app_categorie_new", methods={"GET", "POST"})
+     * @Route("/new", name="app_profile_categorie_new", methods={"GET", "POST"})
      */
     public function new(Request $request, CategorieRepository $categorieRepository): Response
     {
         $categorie = new Categorie();
-        $form = $this->createForm(CategorieType::class, $categorie);
+        $form = $this->createForm(Categorie1Type::class, $categorie);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $categorieRepository->add($categorie);
-            return $this->redirectToRoute('app_categorie_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_profile_categorie_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('categorie/new.html.twig', [
+        return $this->renderForm('profile_categorie/new.html.twig', [
             'categorie' => $categorie,
             'form' => $form,
         ]);
     }
 
     /**
-     * @Route("/{id}", name="app_categorie_show", methods={"GET"})
+     * @Route("/{id}", name="app_profile_categorie_show", methods={"GET"})
      */
     public function show(Categorie $categorie): Response
     {
-        return $this->render('categorie/show.html.twig', [
+        return $this->render('profile_categorie/show.html.twig', [
             'categorie' => $categorie,
         ]);
     }
 
     /**
-     * @Route("/{id}/edit", name="app_categorie_edit", methods={"GET", "POST"})
+     * @Route("/{id}/edit", name="app_profile_categorie_edit", methods={"GET", "POST"})
      */
     public function edit(Request $request, Categorie $categorie, CategorieRepository $categorieRepository): Response
     {
-        $form = $this->createForm(CategorieType::class, $categorie);
+        $form = $this->createForm(Categorie1Type::class, $categorie);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $categorieRepository->add($categorie);
-            return $this->redirectToRoute('app_categorie_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_profile_categorie_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('categorie/edit.html.twig', [
+        return $this->renderForm('profile_categorie/edit.html.twig', [
             'categorie' => $categorie,
             'form' => $form,
         ]);
     }
 
     /**
-     * @Route("/{id}", name="app_categorie_delete", methods={"POST"})
+     * @Route("/{id}", name="app_profile_categorie_delete", methods={"POST"})
      */
     public function delete(Request $request, Categorie $categorie, CategorieRepository $categorieRepository): Response
     {
@@ -83,6 +83,6 @@ class CategorieController extends AbstractController
             $categorieRepository->remove($categorie);
         }
 
-        return $this->redirectToRoute('app_categorie_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_profile_categorie_index', [], Response::HTTP_SEE_OTHER);
     }
 }
